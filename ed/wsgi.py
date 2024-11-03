6a274ce5fba2d11ed738e45bef349ff3452db97e
kabitagorain6 <kabitagorain6@gmail.com>
2024-11-03 08:09:37 +0600

    enhance server security

--- ed/wsgi.py
+++ ed/wsgi.py
@@ -8,9 +8,8 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ed.settings')
-
 application = get_wsgi_application()
