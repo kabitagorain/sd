dc75a29e5a266f5950814d1082bb329945aa27cd
kabitagorain6 <kabitagorain6@gmail.com>
2025-01-16 23:31:50 +0600

    useradmin

--- account/admin.py
+++ account/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from account.models import User
-
-admin.site.register(User)
-
+from django.contrib.auth.admin import UserAdmin
 """
 Registers the User model with the Django admin interface.
 
 This allows the User model to be managed through the Django admin site.
 """
+
+class UserAdmin(UserAdmin):
+    pass
+
+admin.site.register(User, UserAdmin)
